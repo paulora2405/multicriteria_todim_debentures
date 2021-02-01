438c647c1d16cb1d76120efec0ee46f730b0747e
menderson <menderson07@gmail.com>
2021-02-01 15:01:20 -0300

    Fix error 403 on HTTP response

--- scrap.py
+++ scrap.py
@@ -5,12 +5,9 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import json
 
-# ipca = None
-# di = None
 
-
-def getIpca():
-
+def getIpcaIBGE():
+    print("Buscando valor do IPCA:", end='\t')
     url = 'https://www.ibge.gov.br/indicadores'
 
     req = requests.get(url)
@@ -22,58 +19,39 @@ def getIpca():
     ipca = table.split()
     ipca = ipca[5]
     ipca = float(ipca.replace(',', '.'))
+    print(ipca)
+    return ipca
 
+
+def getIpca():
+    print("Buscando valor do IPCA:", end='\t')
+    url = 'https://www.melhorcambio.com/ipca'
+
+    req = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
+
+    soup = BeautifulSoup(req.content, 'html.parser')
+
+    ipca = soup.find('input', id='indice-acum')['value']
+
+    ipca = ipca.split()
+    ipca = ipca[0]
+    ipca = float(ipca.replace(',', '.'))
+    print(ipca)
     return ipca
 
 
 def getCdi():
-
+    print("Buscando valor do CDI:", end='\t')
     url = 'https://www.melhorcambio.com/cdi'
 
-    req = requests.get(url)
+    req = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
 
     soup = BeautifulSoup(req.content, 'html.parser')
 
-    table = soup.find('div', class_='holder-valor')
-    print(soup)
+    cdi = soup.find('input', id='indice-acum')['value']
 
-    # cdi = table.split()
-    # cdi = cdi[5]
-    # cdi = float(cdi.replace(',', '.'))
-
-    # return cdi
-
-
-getCdi()
-# def old():
-
-#    for page in pages:
-#         url = 'https://loja.asus.com.br/ofertas?p=' + page
-
-#         req = requests.get(url)
-
-#         soup = BeautifulSoup(req.content, 'html.parser')
-
-#         lista_produtos = soup.find_all('div', class_='regular')
-
-#         for lista_titulos in lista_produtos:
-#             lista = lista_titulos.find_all('div', class_='product-info')
-
-#             for lista_dados in lista:
-#                 titulo = lista_dados.find(
-#                     'a', class_='product-name').get('title')
-#                 valor = lista_dados.find(
-#                     'p', class_='special-price').find('span', class_='price')
-
-#                 # há dois tipos de estruturas para representar o valor
-#                 if(valor.find('span', class_='price') != None):
-#                     valor = valor.find('span', class_='price').next_element
-#                 else:
-#                     valor = valor.next_element.split(' ')[-1]
-#                     valor = valor.split('\t')[0]
-#                 prices.append(valor)
-#                 titles.append(titulo)
-
-#     df = pd.DataFrame({'price': prices, 'title': titles})
-
-#     df.to_string('products.txt')
+    cdi = cdi.split()
+    cdi = cdi[0]
+    cdi = float(cdi.replace(',', '.'))
+    print(cdi)
+    return cdi
